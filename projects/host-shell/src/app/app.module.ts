b2312ac301427c94eed462ecd94b41becf963975
Shantanu Tiwari <shantiwari35@gmail.com>
2026-07-21 23:13:55 +0530

    added header, footer and aside

--- projects/host-shell/src/app/app.module.ts
+++ projects/host-shell/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { AsideComponent } from './core/layout/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
