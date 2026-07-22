869ae9b0878e4f7b4016bb3a0050dd06f49406b7
Shantanu Tiwari <shantiwari35@gmail.com>
2026-07-22 13:04:39 +0530

    added shared ui with logo

--- projects/shared-ui/src/lib/shared-ui.module.ts
+++ projects/shared-ui/src/lib/shared-ui.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { SharedUiComponent } from './shared-ui.component';
 import { AppIconComponent } from './shared-ui/components/app-icon/app-icon.component';
 import { MaterialModule } from './material/material.module';
+import { LogoComponent } from './shared-ui/components/logo/logo.component';
 
 @NgModule({
-  declarations: [SharedUiComponent, AppIconComponent],
+  declarations: [SharedUiComponent, AppIconComponent, LogoComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [AppIconComponent, MaterialModule],
+  exports: [AppIconComponent, MaterialModule, LogoComponent],
 })
 export class SharedUiModule {}
--- projects/shared-ui/src/public-api.ts
+++ projects/shared-ui/src/public-api.ts
@@ -2,8 +2,16 @@
  * Public API Surface of shared-ui
  */
 
+// Services
 export * from './lib/shared-ui.service';
+
+
+// Components
 export * from './lib/shared-ui.component';
-export * from './lib/shared-ui.module';
 export * from './lib/shared-ui/components/app-icon/app-icon.component';
+export * from './lib/shared-ui/components/logo/logo.component';
+
+
+//modules
+export * from './lib/shared-ui.module';
 export * from './lib/material/material.module';
